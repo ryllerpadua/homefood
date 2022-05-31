f32037ab4a8a1e3ab840687c693a972f7a825c62
ryllerpadua <ryllerpadua@yahoo.com.br>
2022-05-31 14:23:33 -0300

    order controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,25 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
+  def create
+    @product = Product.new(product_params)
+    @order = Order.find(params[:order_id])
+    @product.order = @order
+    if @product.save
+      redirect_to @order
+    else
+      @users = User.all.order(:name)
+      render 'orders/show'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to @product.order
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:category, :title, :description, :price)
   end
 end
