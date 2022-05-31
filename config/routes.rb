51c40c5c196e435b2a460769b9f7edd6f71f4010
Nathalia Vale <nat_sv2@icloud.com>
2022-05-31 18:23:49 -0300

    loginn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products, only: [:index]
   root to: 'pages#home'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :orders do
+    resources :products, only: %i[new create destroy]
+  end
 end
