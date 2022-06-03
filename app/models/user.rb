ea615b1303495c2cd8fc42f9482d210a12ce110f
Francisco Cavalcanti <franciscoreis85@hotmail.com>
2022-06-03 14:30:54 -0300

    Upload de avatar e dropdown menu concluidos. Falta remover foto e mostrar a foto na navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :orders
-  # has_one_attached :photo
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :name, :address
+
+  def avatar_thumbnail
+    if avatar.attached?
+        avatar.variant(resize: "150x150!").processed
+    else
+      "assets/avatar.png"
+    end
+  end
 end
+
+
+# <%= image_tag(current_user.avatar_thumbnail) %>
