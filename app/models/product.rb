3c06a36e22b811dfe5fcdcf71740adf8ea5ed1c8
Nathalia Vale <nat_sv2@icloud.com>
2022-06-03 16:18:19 -0300

    back feito com prof

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_one_attached :photo
 
   validates :category, :title, :description, :price, presence: true
